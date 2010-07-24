4e94a8ba6e039c7b17470961a211ebe984a04c65
sharifpa2009 <sharifpa2009@6e4c8a8f-945f-721b-3526-c4eeb740dfaa>
2010-07-24 02:51:24 +0000

    updated the suggest project as well as registration page

--- register.php
+++ register.php
@@ -33,11 +33,13 @@
 
 <div id="registermaincontent" > 
 
-<form action="verify.php" method="post">
+<form action="" method="post">
 <fieldset id="size">
 <legend id="center">Account Information</legend><br>
-		<b>Username:<font color="red">*</font> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input name="username" type="text" size="50"/><br/></b><br>
-		 <b>Password:<font color="red">*</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="password" name="password" size="50"/></b>
+		<b>Username:<font color="red">*</font> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<input name="username" type="text" size="50"value="<?php echo $_POST['username'] ?>" /><br/></b><br>
+		 <b>Password:<font color="red">*</font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		 <input type="password" name="password" size="50" /></b>
 		 <br><br>
 </fieldset>
 <br/><br>
@@ -46,18 +48,199 @@
 <fieldset>
 <legend id="center"> Personal Information</legend><br>
 		 <b>Full name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		 <input name="fullname" type="text" size="50"/><br/><br>
-         <b>E-mail Address:<font color="red">*</font><input type="text" name="email" size="50"/><br><br>
+		 <input name="fullname" type="text" size="50" value="<?php echo $_POST['fullname'] ?>"/><br/><br>
+         <b>E-mail Address:<font color="red">*
+		 </font><input type="text" name="email" size="50" value="<?php echo $_POST['email'] ?>"/><br><br>
 </fieldset>
 <input type="hidden" name="form_submitted" value="1"/> 
 <br><input type="submit" name="submit" value="Create New Account" />
 </form>
 </div>
 
-
 <!-- end of main Content Wrapper !-->
 </div>
-</div>
+<?php
+
+$servername = 'oniddb.cws.oregonstate.edu';
+$username   = 'sharifpa-db';
+$password	= 'mnsQcoylLEHnTbpb';
+$dbname		= 'sharifpa-db';
+
+$con	    = mysql_connect($servername,$username,$password)
+	or die  ("Error connecting to database server");
+
+	
+mysql_select_db($dbname, $con)
+	or die  ("Error selecting the database");
+
+
+if ($_POST['form_submitted'] == '1') {
+
+		$activationKey =  mt_rand() . mt_rand() . mt_rand();
+		$username = mysql_real_escape_string($_POST[username]);
+		$password = mysql_real_escape_string($_POST[password]);
+		$email = mysql_real_escape_string($_POST[email]);
+		$fullname = mysql_real_escape_string($_POST[fullname]);
+		
+		
+	if (isset($_POST["submit"])){
+	
+	//if user does not enter any information
+		if(empty($username) && empty($password) && empty($email)){
+			?>
+			<script type="text/javascript">
+			alert("Username, Password, and Email Address fileds are required.");	
+			</script>
+			<?php
+			}
+		//if user does not enter passowrd and email
+		elseif(empty($password) && empty($email)){
+			?>
+			<script type="text/javascript">
+			alert("Password and Email Address fields are required.");
+			</script>
+			<?php
+			}
+			//if user does not enter username and email
+		elseif(empty($username) && empty($email)){
+			?>
+			<script type="text/javascript">
+			alert("Username and Email Address fields are required.");
+			</script>
+			<?php
+			}
+		//if user does not enter username and password
+		elseif(empty($username) && empty($password)){
+			?>
+			<script type="text/javascript">
+			alert("Username and Password fields are required.");
+			</script>
+			<?php
+			}
+		
+		elseif(empty($username)){
+			?>
+			<script type="text/javascript">
+			alert("Username is required.");	
+			</script>
+			<?php
+			}
+		elseif(empty($password)){
+			?>
+			<script type="text/javascript">
+			alert("Password is required.");	
+			</script>
+			<?php
+			}
+		elseif(empty($email)){
+			?>
+			<script type="text/javascript">
+			alert("Email Address is reuired.");	
+			</script>
+			<?php
+			}
+	
+		$duplicate0= mysql_query("select username from users where username= '".$username."'");
+		if(mysql_num_rows($duplicate0) > 0){
+			?>
+			<script type="text/javascript">
+			alert("The USERNAME you have specified has already been taken. Please choose a different username.");
+			</script>
+			<?php
+			exit();
+			}
+			
+		$duplicate= mysql_query("select email from users where email= '".$email."'");
+		if(mysql_num_rows($duplicate) > 0){
+			?>
+			<script type="text/javascript">
+			alert("The EMAIL ADDRESS you have specified has already been taken. Please choose a different email adress.");
+			</script>
+			<?php
+			exit();
+			}
+			
+		if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)){ 
+			?>
+			<script type="text/javascript">
+			alert("Invalid Email Address.");
+			</script>
+			<?php
+			unset($email);
+			exit();
+			}
+	
+		$password=md5($pssword);
+		##User is registering, insert data until we can activate it
+
+
+		$sql="INSERT INTO users (username, password, email, fullname, activationkey, status) VALUES ('$username', '$password', '$email', '$fullname', '$activationKey', 'verify')";
+
+		if (!mysql_query($sql)){
+			die('Error: ' . mysql_error());
+		}
+		
+			?>
+			<script type="text/javascript">
+			alert("An email has been sent to your email address with an activation key. Please check your mail to complete registration.");
+			</script>
+			<?php
+		//echo "An email has been sent to $_POST[email] with an activation key. Please check your mail to complete registration.";
+
+		##Send activation Email
+
+		$to      = $_POST[email];
+
+		$subject = " YOURWEBSITE.com Registration";
+
+		$message = "Welcome to our website!\r\rYou, or someone using your email address, has completed registration at YOURWEBSITE.com. You can complete registration by clicking the following link:\rhttp://web.engr.orst.edu/~sharifpa/HOSS/verify.php?$activationKey
+		\r\rIf this is an error, ignore this email and you will be removed from our mailing list.\r\rRegards,\ YOURWEBSITE.com Team";
+
+		$headers = 'From: noreply@ YOURWEBSITE.com' . "\r\n" .
+
+		'Reply-To: noreply@ YOURWEBSITE.com' . "\r\n" .
+
+		'X-Mailer: PHP/' . phpversion();
+
+		mail($to, $subject, $message, $headers);
+
+	} 
+}
+/*
+else {
+
+##User isn't registering, check verify code and change activation code to null, status to activated on success
+
+$queryString = $_SERVER['QUERY_STRING'];
+
+$query = "SELECT * FROM users"; 
+
+$result = mysql_query($query) or die(mysql_error());
+
+while($row = mysql_fetch_array($result)){
+
+if ($queryString == $row["activationkey"]){
+		?>
+			<script type="text/javascript">
+			alert("Congratulations! You have successfully activated your account.");	
+			</script>
+			<?php
+       $sql="UPDATE users SET activationkey = '', status='Activated' WHERE (id = $row[id])";
+	
+       if (!mysql_query($sql)){
+			die('Error: ' . mysql_error());
+		}
+			
+	}
+
+  }
+
+}
+*/
+?>
+
+
+
 
 </body>
 </html>
--- verify.php
+++ verify.php
@@ -28,11 +28,9 @@
 </ul>
 
 <div id="title">
-	<a href="register.php"> Go back to registration page</a><br>
-	<a href="index.html"> Go to main page</a>
+	<center><br>Congratulations! You have successfully activated your account.<br><br>
+	<a href="index.html"> Go to main page</a></center>
 </div>
-
-
 </body>
 </html>
 
@@ -49,130 +47,7 @@ $con	    = mysql_connect($servername,$username,$password)
 	
 mysql_select_db($dbname, $con)
 	or die  ("Error selecting the database");
-
-
-if ($_POST['form_submitted'] == '1') {
-
-		$activationKey =  mt_rand() . mt_rand() . mt_rand();
-		$username = mysql_real_escape_string($_POST[username]);
-		$password = mysql_real_escape_string($_POST[password]);
-
-		$email = mysql_real_escape_string($_POST[email]);
-		
-		
-	if (isset($_POST["submit"])){
-
 	
-	
-	//if user does not enter any information
-		if(empty($username) && empty($password) && empty($email)){
-			?>
-			<script type="text/javascript">
-			alert("Username, Password, and Email Address fileds are required.");	
-			</script>
-			<?php
-			}
-		//if user does not enter passowrd and email
-		elseif(empty($password) && empty($email)){
-			?>
-			<script type="text/javascript">
-			alert("Password and Email Address fields are required.");
-			</script>
-			<?php
-			}
-			//if user does not enter username and email
-		elseif(empty($username) && empty($email)){
-			?>
-			<script type="text/javascript">
-			alert("Username and Email Address fields are required.");
-			</script>
-			<?php
-			}
-		//if user does not enter username and password
-		elseif(empty($username) && empty($password)){
-			?>
-			<script type="text/javascript">
-			alert("Username and Password fields are required.");
-			</script>
-			<?php
-			}
-		
-		elseif(empty($username)){
-			?>
-			<script type="text/javascript">
-			alert("Username is required.");	
-			</script>
-			<?php
-			}
-		elseif(empty($password)){
-			?>
-			<script type="text/javascript">
-			alert("Password is required.");	
-			</script>
-			<?php
-			}
-		elseif(empty($email)){
-			?>
-			<script type="text/javascript">
-			alert("Email Address is reuired.");	
-			</script>
-			<?php
-			}
-	
-		
-		
-		$checkemail = mysql_query ("select Email from users where Email = '$email'");
-		$email_exist = mysql_num_rows($checkemail);
-		//if email already exists in database. 
-		if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)){ 
-			?>
-			<script type="text/javascript">
-			alert("Invalid Email Address.");
-			</script>
-			<?php
-			unset($email);
-			exit();
-			}
-	
-		$password=md5($pssword);
-		##User is registering, insert data until we can activate it
-
-		
-
-		$sql="INSERT INTO users (username, password, email, fullname, activationkey, status) VALUES ('$username', '$password', '$email', '$fullname', '$activationKey', 'verify')";
-
-		if (!mysql_query($sql)){
-			die('Error: ' . mysql_error());
-		}
-		
-			?>
-			<script type="text/javascript">
-			alert("An email has been sent to your email address with an activation key. Please check your mail to complete registration.");
-			</script>
-			<?php
-		//echo "An email has been sent to $_POST[email] with an activation key. Please check your mail to complete registration.";
-
-		##Send activation Email
-
-		$to      = $_POST[email];
-
-		$subject = " YOURWEBSITE.com Registration";
-
-		$message = "Welcome to our website!\r\rYou, or someone using your email address, has completed registration at YOURWEBSITE.com. You can complete registration by clicking the following link:\rhttp://web.engr.orst.edu/~sharifpa/HOSS/verify.php?$activationKey
-		\r\rIf this is an error, ignore this email and you will be removed from our mailing list.\r\rRegards,\ YOURWEBSITE.com Team";
-
-		$headers = 'From: noreply@ YOURWEBSITE.com' . "\r\n" .
-
-		'Reply-To: noreply@ YOURWEBSITE.com' . "\r\n" .
-
-		'X-Mailer: PHP/' . phpversion();
-
-		mail($to, $subject, $message, $headers);
-
-	} 
-}
-else {
-
 ##User isn't registering, check verify code and change activation code to null, status to activated on success
 
 $queryString = $_SERVER['QUERY_STRING'];
@@ -181,30 +56,16 @@ $query = "SELECT * FROM users";
 
 $result = mysql_query($query) or die(mysql_error());
 
-  while($row = mysql_fetch_array($result)){
+while($row = mysql_fetch_array($result)){
 
-    if ($queryString == $row["activationkey"]){
-		
-			?>
-			<script type="text/javascript">
-			alert("Congratulations! You have successfully activated your account.");	
-			</script>
-			<?php
-       echo "Congratulations!" . $row["username"] . " You have successfully activated your account. ";
+if ($queryString == $row["activationkey"]){
 
-       $sql="UPDATE users SET activationkey = '', status='activated' WHERE (id = $row[id])";
-
-       if (!mysql_query($sql))
-
-  {
-
-        die('Error: ' . mysql_error());
+    $sql="UPDATE users SET activationkey = '', status='Activated' WHERE (id = $row[id])";
+      if (!mysql_query($sql)){
+			die('Error: ' . mysql_error());
+		}
+			
+	}
 
   }
 
-    }
-
-  }
-
-}
-?>
